0ad9b28f9251bcabbe2f9d0f3aef1883a8c70947
MikesMikes <michaelkaliber15@gmail.com>
2022-01-19 16:27:44 +0000

    Added POJOS. Closes #2.

---
+++ src/main/java/com/MikesMikes/mkmpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package com.MikesMikes.mkmpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/com/MikesMikes/mkmpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package com.MikesMikes.mkmpetclinic.model;
+
+public class Vet extends Person{
+}
