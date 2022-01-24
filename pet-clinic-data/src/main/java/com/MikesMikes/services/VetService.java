cb5e60b0ff9ddc8f5abcdf88fca1be6f52a1e4fe
MikesMikes <michaelkaliber15@gmail.com>
2022-01-24 15:22:27 +0000

    refactor to common crud service.

--- pet-clinic-data/src/main/java/com/MikesMikes/services/VetService.java
+++ pet-clinic-data/src/main/java/com/MikesMikes/services/VetService.java
@@ -2,13 +2,6 @@ package com.MikesMikes.services;
 
 import com.MikesMikes.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
-    Vet findById(Long id);
-
-    Vet save(Vet vet);
-
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
 
 }
