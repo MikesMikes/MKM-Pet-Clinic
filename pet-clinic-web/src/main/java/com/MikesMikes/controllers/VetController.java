93008dc64284ca7e8bf80df421ff3c0ea35f4189
MikesMikes <michaelkaliber15@gmail.com>
2022-01-26 15:37:40 +0000

    Added Owner Controller and index page. Closes #12

---
+++ pet-clinic-web/src/main/java/com/MikesMikes/controllers/VetController.java
@@ -0,0 +1,16 @@
+package com.MikesMikes.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class VetController {
+
+    @RequestMapping({"vets", "vets.html","/vets/index.html","/vets/index"})
+    public String listVets(){
+
+
+
+        return "vets/index";
+    }
+}
