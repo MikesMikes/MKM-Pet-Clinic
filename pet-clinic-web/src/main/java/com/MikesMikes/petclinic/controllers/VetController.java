626f98dcb04dae6a9a8cd28984125592c5e5189a
MikesMikes <michaelkaliber15@gmail.com>
2022-02-22 19:40:52 +0000

    Refactor to automated ID creation. Closes #23

--- pet-clinic-web/src/main/java/com/MikesMikes/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/MikesMikes/petclinic/controllers/VetController.java
@@ -1,15 +1,24 @@
 package com.MikesMikes.petclinic.controllers;
 
+import com.MikesMikes.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"vets", "vets.html","/vets/index.html","/vets/index"})
-    public String listVets(){
+    public String listVets(Model model){
 
-
+        model.addAttribute("vets", vetService.findAll());
 
         return "vets/index";
     }
