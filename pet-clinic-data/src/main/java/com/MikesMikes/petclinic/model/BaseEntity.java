3c7ada73d09cedacdc41366f82f2c8d5280e93da
MikesMikes <michaelkaliber15@gmail.com>
2022-03-23 16:30:07 +0000

    Updated Base Entity with JPA annotations. Closes #35

--- pet-clinic-data/src/main/java/com/MikesMikes/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/MikesMikes/petclinic/model/BaseEntity.java
@@ -1,9 +1,14 @@
 package com.MikesMikes.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
