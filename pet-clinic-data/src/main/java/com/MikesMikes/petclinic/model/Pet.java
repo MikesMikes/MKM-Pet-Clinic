8bcda7fc752e5b92f16d36970c46960ccc372065
MikesMikes <michaelkaliber15@gmail.com>
2022-03-24 19:00:34 +0000

    Created Visit entity with JPA, associated to Pet. Closes #38

--- pet-clinic-data/src/main/java/com/MikesMikes/petclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/com/MikesMikes/petclinic/model/Pet.java
@@ -2,6 +2,8 @@ package com.MikesMikes.petclinic.model;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -21,8 +23,21 @@ public class Pet extends BaseEntity{
     @Column(name = "name")
     private String name;
 
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
+
+
 
     //getters setters
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
+    }
+
     public String getName() {
         return name;
     }
