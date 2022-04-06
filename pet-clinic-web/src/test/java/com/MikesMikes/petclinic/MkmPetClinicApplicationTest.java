90d9d0f3eafa092d90caf25721272bf4ab3be943
MikesMikes <michaelkaliber15@gmail.com>
2022-04-06 23:15:27 +0100

    Restricted JUNit 4. Moved to JUnit 5. Closes #4

---
+++ pet-clinic-web/src/test/java/com/MikesMikes/petclinic/MkmPetClinicApplicationTest.java
@@ -0,0 +1,20 @@
+package com.MikesMikes.petclinic;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class MkmPetClinicApplicationTest {
+
+    @Test
+    void main() {
+    }
+
+    @Test
+    public void contextLoads(){
+
+    }
+}
